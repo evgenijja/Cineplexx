0b3a9859e08d9b5275876cfcb16625391937c902
evgenijja <42324122+evgenijja@users.noreply.github.com>
2020-05-22 20:42:26 +0200

    Add files via upload

---
+++ auth.py
@@ -0,0 +1,10 @@
+# Skopiraj datoteko v auth.py ali auth.R in vnesi podatke za priklop na bazo
+db = 'sem2020_evgenijab'
+host = 'baza.fmf.uni-lj.si'
+user = 'evgenijab'
+password = 't9eg3d07'
+
+##db = 'sem2020_karmenz'
+##host = 'baza.fmf.uni-lj.si'
+##user = 'karmenz'
+##password = '0mag5baa'
